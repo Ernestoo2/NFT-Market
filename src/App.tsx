6af31670c3b70b43d85d67e84761e3f846389a1b
Eze Ernest Chibueze <ezechibuezeernest@gmail.com>
2024-12-05 10:35:16 +0100

    Submission before deadline second  trial push

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import "./styles/Global.css";
+import DetailedPageUi from "./ui/DetailedPageUi";
 import ListingPageUi from "./ui/ListingPageUi";
 import React from "react";
 
@@ -8,7 +9,7 @@ function App() {
     <div className="App ">
       
       <ListingPageUi/>
-      {/* <DetailedPageUi/> */}
+      <DetailedPageUi/>
       
     </div>
   );
