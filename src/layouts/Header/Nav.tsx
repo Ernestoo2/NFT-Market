8c1bec1b1c31cd9cb480add6666bbc4ac6a83d62
Eze Ernest Chibueze <ezechibuezeernest@gmail.com>
2024-12-05 10:28:03 +0100

    Submission before deadline second  trial push

--- src/layouts/Header/Nav.tsx
+++ src/layouts/Header/Nav.tsx
@@ -5,10 +5,10 @@ export class Nav extends Component {
     return (
       <div className=" text-[#7780A1] ">
         <nav className="w-full lg:w-[523px] lg:h-[20px] lg:gap-[64px] sm:w-full md:w-[400px] md:text-sm sm:pr-2 h-[20px]  flex justify-evenly ">
-            <a className="" href="#">Auction</a>
-            <a className="" href="#">Roadmap</a>
-            <a className="" href="#">Discover</a>
-            <a className="" href="#">Community</a>
+            <a className="" href="/">Auction</a>
+            <a className="" href="/">Roadmap</a>
+            <a className="" href="/">Discover</a>
+            <a className="" href="/">Community</a>
         </nav>
       </div>
     )
