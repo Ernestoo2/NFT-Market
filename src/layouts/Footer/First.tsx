7d845e69915ec6a83bda4a8ffa34e26964bb2c04
Eze Ernest Chibueze <ezechibuezeernest@gmail.com>
2024-12-05 10:23:19 +0100

    Trying to resolve potential issues

--- src/layouts/Footer/First.tsx
+++ src/layouts/Footer/First.tsx
@@ -8,10 +8,10 @@ export class First extends Component {
        <img src="/Assets/Logo.png" alt="for logo" className="fill-white"/>
        <h3 className="w-[160px] h-[20px] text-white font-semibold">NFT MARKET</h3>
        </div>
-       <div className="w-[294px] flex justify-evenly mt-[190px] mt-[40px] text-white">
-       <a href="#">Support</a>
-       <a href="#">Terms of service </a>
-       <a href="#">Licence</a>
+       <div className="w-[294px] flex justify-evenly mt-[190px]  text-white">
+       <a href="/">Support</a>
+       <a href="/">Terms of service </a>
+       <a href="/">Licence</a>
        </div>
       </div>
     )
