6f2eb823dd4ee646502454e23d10e7ba97117d0d
Eze Ernest Chibueze <ezechibuezeernest@gmail.com>
2024-12-09 19:51:19 +0100

    solved basic problems now testing deployment

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App ">
       
-      {/* <ListingPageUi/> */}
+      <ListingPageUi/>
       <DetailedPageUi/>
       
     </div>
--- src/ui/HeaderUi.tsx
+++ src/ui/HeaderUi.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 function HeaderUi() {
   return (
-    <div className="w-full max-w-full sm:max-w-3/4  lg:max-w-full  flex justify-between items-center px-4 py-2 mx-auto">
+    <div className="w-full shadow bg-[#2a32a3] max-w-full sm:max-w-3/4  lg:max-w-full  flex justify-between items-center px-4 py-2 mx-auto">
       <Logo />
       <Nav />
       <Endside />
